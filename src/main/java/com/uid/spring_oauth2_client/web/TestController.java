a53aa448b53668ae9fb4bd2098e389b72d032749
maatoub <abdenasser.hasnaoui@edu.uiz.ac.ma>
2024-09-05 18:17:00 +0100

    first commit

---
+++ src/main/java/com/uid/spring_oauth2_client/web/TestController.java
@@ -0,0 +1,13 @@
+package com.uid.spring_oauth2_client.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String home() {
+        return "Welcome to the OAuth application";
+    }
+}
